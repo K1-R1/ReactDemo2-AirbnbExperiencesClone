6fbda3d34f006a983af65abcc49275ab732f3eb8
K1-R1 <kyranrawlinson@gmail.com>
2022-03-12 22:08:27 +0000

    Added basic components

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import logo from "../img/download.png";
+import "../style.css"
+
+function Navbar() {
+    return (
+        <nav>
+            <img src={logo} />
+        </nav>
+    )
+}
+
+export default Navbar;
