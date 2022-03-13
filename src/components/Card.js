63d6ae2668623c935dfe1610ae82f1cd8127fa21
K1-R1 <kyranrawlinson@gmail.com>
2022-03-13 20:27:27 +0000

    Refactored Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,24 +3,24 @@ import "../style.css"
 function Card(props) {
 
     let badgeText
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "ONLINE"
     }
 
     return (
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img className="card--image" src={`img/${props.img}`} alt="img" />
+            <img className="card--image" src={`img/${props.item.coverImg}`} alt="img" />
             <div className="card--stats">
                 <img className="card--star" src="img/star.png" alt="img" />
-                <span>{props.rating}</span>
-                <span className="grey">({props.reviewCount}) • </span>
-                <span className="grey">{props.location}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="grey">({props.item.stats.reviewCount}) • </span>
+                <span className="grey">{props.item.location}</span>
             </div>
-            <p className="card--title">{props.title}</p>
-            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.item.title}</p>
+            <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
     )
 }
