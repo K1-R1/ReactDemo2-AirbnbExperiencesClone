78c3eb3e8f2c2591bb191d87887be13e52a004e9
K1-R1 <kyranrawlinson@gmail.com>
2022-03-13 20:16:10 +0000

    Updated Card component with contextual rendering

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,17 @@
 import "../style.css"
 
 function Card(props) {
+
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img className="card--image" src={`img/${props.img}`} alt="img" />
             <div className="card--stats">
                 <img className="card--star" src="img/star.png" alt="img" />
