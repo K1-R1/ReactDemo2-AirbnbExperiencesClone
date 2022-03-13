c7492ac7f0fbc51dae5020a0ca378b5816504cc6
K1-R1 <kyranrawlinson@gmail.com>
2022-03-13 20:06:42 +0000

    Updated src/components/Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,10 +8,10 @@ function Card(props) {
                 <img className="card--star" src="img/star.png" alt="img" />
                 <span>{props.rating}</span>
                 <span className="grey">({props.reviewCount}) • </span>
-                <span className="grey">{props.country}</span>
+                <span className="grey">{props.location}</span>
             </div>
-            <p>{props.title}</p>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
